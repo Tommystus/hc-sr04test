607334e779158727ad18d6008fe1c7baadf6d9d8
Tommystus <tommystus@gmail.com>
2018-02-05 23:20:01 -0700

    Testing multi-trigger

--- fs/init.js
+++ fs/init.js
@@ -29,6 +29,46 @@ GPIO.set_mode(trigPin, GPIO.MODE_OUTPUT);
 GPIO.write(trigPin,0);
 
 
+/* Complext use.
+	Try not to use global var in callback method.
+	Use user object to pass data around for stability
+*/
+let myData = {
+	deviceAvailable: true,
+	dataReady: false,
+	icnt: 1,
+	tacc: 0,
+	tcnt: 0,
+	tavg: 0,
+	doTrig: function(t, arg) {
+		arg.tacc += t;
+		arg.tcnt++;
+//		print('**m-echoCb t=', t, 'tacc=', arg.tacc, 'tcnt=', arg.tcnt);
+		if (arg.icnt > 1) {
+			arg.icnt--;
+			Timer.set(10, false, function(arg) {
+				hcTrigEchoCb(trigPin, arg.doTrig, arg);
+			},arg);
+		}
+		else {
+			arg.tavg = arg.tacc/arg.tcnt;
+			print('**m-echoCb tavg=', arg.tavg, 'tcnt=', arg.tcnt);
+			arg.deviceAvailable = true;
+			arg.dataReady = true;
+		}
+	},
+
+	startMultiTrig: function(icnt) {
+		this.icnt = icnt;
+		this.tacc = 0;
+		this.tcnt = 0;
+		this.deviceAvailable = false;
+		this.dataReady = false;
+		hcTrigEchoCb(trigPin, this.doTrig, this);
+	}
+};
+
+
 // Note:  mos console command reset the GPIO edge detection
 // so it may trigger without button press
 GPIO.set_button_handler(btnPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
@@ -38,88 +78,34 @@ GPIO.set_button_handler(btnPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function()
 
 	print('**Button Trigger');
 
-/* ok but not great
-	Timer.set(200, false, function() {
-		let t = hcGetEchoTime();
-		print('**echo t=', t);	// result from last trigger
-// 		fire trigger without callback
-		hcTrigEcho(trigPin);	// trigger for next cycle
-	}, null);
-*/
 
 /*	use call back method is stable
-*/
 	hcTrigEchoCb(trigPin, function( t, arg) {
-		print('**b-echoCb t=', t);
-	}, null);
+		print('**b-echoCb t=', t, 'arg=', arg);
+	}, 2);
+*/
 
-/* multiple trig to get avg doesn't work
-	let tcnt = 2;
-	let tacc = 0;
-	for (let i=0; i < tcnt; i++) {
-		hcTrigEchoCb(trigPin, function( t, arg) {
-			tacc += t;
-		}, null);
-		Sys.usleep(200000);
-	}
-	let t = tacc/tcnt;
-	print('**bt-echo avg t=', t);
-*/
-/* also unstable
-	let tacc = 0;
-	let tcnt = 4;
-	for (let i=0; i < tcnt; i++) {
-		let waitForEcho = true;
-		hcTrigEchoCb(trigPin, function( t, arg) {
-			tacc += t;
-			waitForEcho = false;
-		}, null);
-		while (waitForEcho) {
-			Sys.wdt_feed();
-		}
-	}
-	let t = tacc/tcnt;
-	print('**bt-echo avg t=', t);
-*/
+/*  Use user data obj to do multiple trigger */
+	myData.startMultiTrig(4);
 
 }, null);
 
 
-Timer.set(4000, true, function() {
-
-/* This is ok - only touch gpio before trigger
-	GPIO.write(ledPin,0);
-	Timer.set(100, false, function() {
-		let t = hcGetEchoTime();
-		print('**t-echo t=', t);	// result from last trigger
-		GPIO.write(ledPin,1);
-		// stable trigger here
-		hcTrigEcho(trigPin);	// trigger for next cycle
-	}, null);
-*/
+Timer.set(1000, true, function() {
 
 /* single trigger with callback is ok
-*/
-	GPIO.write(ledPin,0);	// signal LED - use gpio is ok before trigger
-	hcTrigEchoCb(trigPin, function( t, arg) {
-		print('**t-echo t=', t);
-		GPIO.write(ledPin,1); 	// touch gpio after echo is ok
-	}, null);
-
-/* Multiple trigger with wait delay to do echo avg is unstable
-	let tcnt = 2;
-	let tacc = 0;
-	for (let i=0; i < tcnt; i++) {
+	if (myData.deviceAvailable) {
+		GPIO.write(ledPin,0);	// signal LED - use gpio is ok before trigger
 		hcTrigEchoCb(trigPin, function( t, arg) {
-			tacc += t;
+			print('**t-echo t=', t);
+			GPIO.write(ledPin,1); 	// touch gpio after echo is ok
 		}, null);
-		Sys.usleep(200000);
 	}
-	let t = tacc/tcnt;
-	print('**t1a-echo avg t=', t);
 */
-
-//	GPIO.write(ledPin,1); // don't touch gpio during trigger operation
+	if (myData.dataReady) {
+		print('**t-echoCb tavg=', myData.tavg, 'tcnt=', myData.tcnt);
+		myData.dataReady = false;
+	}
 
 }, null);
 
