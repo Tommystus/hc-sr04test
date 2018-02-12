fc7ca1ddd19319e3a464fcd9c64c3ff7e9f6ba28
Tommystus <tommystus@gmail.com>
2018-02-11 22:24:59 -0700

    remove unused api

--- fs/init.js
+++ fs/init.js
@@ -4,9 +4,6 @@
 	pulse measurement is done using gpio pin interrupt handler
 */
 load('api_gpio.js');
-load('api_mqtt.js');
-load('api_events.js');
-load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
 
